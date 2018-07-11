df6b6c616dac827558590ce8e2da4a1e857ccad8
nal13 <nunoalexgouv@hotmail.com>
2018-07-11 14:32:14 +0100

    guide 6 complete

--- mp/settings.py
+++ mp/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'widget_tweaks',
-    
+
     'boards',
     'accounts'
 ]
